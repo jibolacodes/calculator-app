37785c512d234cd00587b58cb8367e3a2ee98152
Ajibola Akindunbi <jibzsupreme@gmail.com>
2022-02-14 14:53:41 +0100

    'js'

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 const body = document.querySelector('.body');
 const input = document.querySelector('.input');
 const toggleBtn = document.querySelector('.toggle-btn');
-const switchBtn = document.querySelector('.switch');
 const display = document.getElementById('display');
+const switchBtns = document.querySelectorAll('.switch');
 const btns = document.querySelectorAll('.btn');
 
 display.value = 0;
-display.value.lemgth = 9;
-
 
 btns.forEach(function(btn){
 	btn.addEventListener('click', function(e){
@@ -33,27 +31,32 @@ btns.forEach(function(btn){
 			if(displayedNum.includes('.')){
 				display.value = displayedNum;
 			}
-			input.dataset.prevSymbol = 'decimal'
+			input.dataset.prevSymbol = 'decimal';
+		}
+
+		if(element === 'reset'){
+			display.value = 0;
+			input.dataset.prevSymbol = 'reset';
 		}
 
 		if(element === 'add' || 
 			element === 'subtract' || 
 			element === 'multiply' || 
-			element === 'divide' ||
-			element === 'reset'){
+			element === 'divide')
+		{
 
 			const operator = input.dataset.operator;
 			const firstValue = input.dataset.firstValue;
 			const secondValue = displayedNum;
 
-			// if(firstValue && operator){
-			// 	display.value = calculate(firstValue, operator, secondValue);
-			// }
+			if(firstValue && operator && prevSymbol !== 'operator'){
+				display.value = calculate(firstValue, operator, secondValue);
+			}
 
 			// //creates new custom attribute in input
-			// input.dataset.operator = element;
-			// input.dataset.firstValue = displayedNum;
-			// input.dataset.prevSymbol = 'operator';
+			input.dataset.firstValue = displayedNum;
+			input.dataset.prevSymbol = 'operator';
+			input.dataset.operator = element;
 		}
 
 		if(element === 'calculate'){
@@ -62,13 +65,14 @@ btns.forEach(function(btn){
 			const secondValue = displayedNum;
 
 			display.value = calculate(firstValue, operator, secondValue);
-			input.dataset.prevSymbol = 'calculate'
+			input.dataset.prevSymbol = 'calculate';
 		}
 
-		if(element === 'reset'){
-			display.value = 0
+		if(element === 'delete'){
+			let number = display.value;
+			number = number.slice(0, -1);
+			display.value = number;
 		}
-
 	})
 })
 	
@@ -87,3 +91,27 @@ function calculate(a,operator,b){
 	}
 	return result;
 }
+
+const redBtn = document.querySelector('.switch-a');
+
+switchBtns.forEach(function(switchBtn){
+	switchBtn.addEventListener('click', function(e){
+		const id = e.target.dataset.id
+		console.log(id)
+		if(id === '1'){
+			redBtn.style.left = '0';
+			body.classList.add('theme-a');
+			body.classList.remove('theme-b', 'theme-c');
+		}
+		if(id === '2'){
+			redBtn.style.left = '20px';
+			body.classList.add('theme-b');
+			body.classList.remove('theme-a', 'theme-c');
+		}
+		if(id === '3'){
+			redBtn.style.left = '40px';
+			body.classList.add('theme-c');
+			body.classList.remove('theme-a', 'theme-b');
+		}
+	})
+})
---
+++ src/note.md
@@ -0,0 +1,42 @@
+/*.toggle-btn {
+  background-color: var(--clr-bg-toggle);
+  padding: 3px 24px;
+  position: relative;
+  bottom: 3px;
+  border: none;
+  border-radius: 8px;
+  display: flex;
+  justify-content: space-between;
+}*/
+/*.switch {
+  background-color: var(--clr-key-toggle-bg);
+  border: none;
+  border-radius: 50%;
+  width: 12px;
+  height: 12px;
+  position: relative;
+}*/
+/*.select-one {
+  position: relative;
+  left: -18px;
+}
+.select-two {
+  position: relative;
+  left: 0;
+}
+.select-three {
+  position: relative;
+  left: 18px;
+}*/
+
+
+/*.btn-2 {
+  height: 2.5rem;
+  font-family: inherit;
+  font-weight: var(--fw);
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  min-height: 40px;
+  border: none;
+  border-radius: var(--br);
+}*/
